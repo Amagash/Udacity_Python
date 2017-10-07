b984f4dd1e90bab1494287ca1511515e367589b4
Amagash <tiffanysouterre@gmail.com>
2017-10-07 17:44:14 +0200

    fibonacci_non recursive
    #Define a faster fibonacci procedure that will enable us to computer
    #fibonacci(36).

--- fibonacci.py
+++ fibonacci.py
@@ -8,16 +8,27 @@
 # Recursive Case:
 #    n > 1 : fibonacci(n) => fibonacci(n-1) + fibonacci(n-2)
 
-def fibonacci(n):
+def fibonacci_recursive(n):
     if n == 0:
         return 0
     elif n == 1:
         return 1
-    elif n > 1:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+    return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
 
 
+#Define a faster fibonacci procedure that will enable us to computer
+#fibonacci(36).
 
+def fibonacci(n):
+    current = 0
+    after = 1
+    for i in range(0,n):
+        current, after = after, current + after
+    return current
+
+
+print fibonacci(36)
+#>>> 14930352
 
 
 print fibonacci(0)
