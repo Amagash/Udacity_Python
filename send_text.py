a8d5b369ab13d811aa0e9c8991e90d44ff53f77b
TIffany Souterre <tiffanysouterre@gmail.com>
2017-11-08 12:04:27 +0100

    change send_text

--- send_text.py
+++ send_text.py
@@ -2,15 +2,15 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "ACa467ed678a2067bf67ec284dbbf55c2e"
+account_sid = "SID"
 # Your Auth Token from twilio.com/console
-auth_token  = "df59ff35658616dee86c228a96ac7abf"
+auth_token  = "author token"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+33621110251",
-    from_="+33756799395",
+    to="phone number",
+    from_="phone number",
     body="coucou ! ")
 
 print(message.sid)
